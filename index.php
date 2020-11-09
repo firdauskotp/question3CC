17a1af917dd350790c2d16695d1ad4484ea6c21d
Muhammad Firdaus <46307489+firdauskotp@users.noreply.github.com>
2020-11-09 17:05:30 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php 
-   $my_string = 'Code to display text'; 
+   $my_string = 'Hello Guys, I am Muhammad Firdaus bin Badauraudine, 52221118462, the Cloud Computer Expert! Pleasego to my GitHub account : https://github.com/firdauskotp '; 
    echo $my_string; 
 ?> 
